d3d689af5403043122a19db4725e08f7068fd273
AlexanderDerkun <38619024+AlexanderDerkun@users.noreply.github.com>
2023-07-27 10:44:09 +0300

    add OnClickListener to a fragment's button

--- app/src/main/java/com/derkun/secretmessage/WelcomeFragment.kt
+++ app/src/main/java/com/derkun/secretmessage/WelcomeFragment.kt
@@ -1,10 +1,16 @@
 package com.derkun.secretmessage
 
+/**
+ * @author A.V.Derkun
+ * @time 10:38 a.m. 27.07.2023
+ */
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.findFragment
 
 
 class WelcomeFragment : Fragment() {
@@ -12,8 +18,17 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        //  В этом месте получаем ссылку на корневое представление фрагмента
+        val view =  inflater.inflate(R.layout.fragment_welcome, container, false)
+        // Используем корневое представление с целью получения ссылку для кнопки старта фрагмента
+        val startButton = view.findViewById<Button>(R.id.start)
+
+        //Вызываем setOnClickListener для startButton
+        startButton.setOnClickListener {
+            // Запишем позже код который будет вызываться, когда по кнопке старта нажмут
+        }
+        // Возвращаем ссылку на корневое представление
+        return view
     }
 
 }
