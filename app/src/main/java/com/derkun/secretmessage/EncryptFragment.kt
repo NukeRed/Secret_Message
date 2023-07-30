84fd5d6890e9dbeabe80ae82f7e45595e3d4ed24
AlexanderDerkun <38619024+AlexanderDerkun@users.noreply.github.com>
2023-07-30 15:38:30 +0300

    update the EncryptFragment.kt

--- app/src/main/java/com/derkun/secretmessage/EncryptFragment.kt
+++ app/src/main/java/com/derkun/secretmessage/EncryptFragment.kt
@@ -9,17 +9,15 @@ import android.widget.TextView
 
 class EncryptFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_encrypt, container, false)
-
-
-        val message = EcryptFragmentArgs.fromBundle(reguireArguments()).message
-        val encyptedView = view.findViewById<TextView>(R.id.encrypted_message)
-        encryptedView.text = message.reversed()
-        return view
-    }
+   override fun onCreateView(
+       inflater: LayoutInflater,
+       container: ViewGroup?,
+       savedInstanceState: Bundle?
+   ): View? {
+       val view = inflater.inflate(R.layout.fragment_encrypt,container,false)
+       val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+       val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
+       encryptedView.text = message.reversed()
+       return view
+   }
 }
